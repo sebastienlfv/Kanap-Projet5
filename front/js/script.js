705348f6508f9f9dc6f0291fd1e8d5a52bb1e928
sebastienlfv <sebastienlfv.pro@gmail.com>
2022-03-11 19:15:08 +0100

    *

--- front/js/script.js
+++ front/js/script.js
@@ -1,11 +1,12 @@
 
+
 main();
 
 async function main() {
-  featchKanaps();
+  fetchKanaps();
 }
 
-function featchKanaps() {
+function fetchKanaps() {
   const URL_API = "http://www.localhost:3000/api/products"
   fetch(URL_API)
     .then(function(res) {
@@ -13,21 +14,21 @@ function featchKanaps() {
         return res.json();
       }
     })
-    .then(function(value){
-      console.log(value);
+    .then(function(value) {
+      console.log(value)
       displayKanap(value)
     })
-    .catch(function(err){
+    .catch(function(err) {
 
-    })
+    });
 }
 
 function displayKanap(dataFromApi) {
   const kanapElement = document.querySelector('#items')
 
   for (let i = 0; i < dataFromApi.lenght; i++){
-    let paraprah = document.createElement("p");
-    paraprah.innerHTML = dataFromApi[i].name;
-    kanapElement.appendChild(paraprah)
+    let paragraph = document.createElement("p");
+    paragraph.innerHTML = dataFromApi[i].name;
+    kanapElement.appendChild(paragraph)
   }
 }
