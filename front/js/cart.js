bfee8f1dd9e0635ded6813f20762ac8397fcaded
sebastienlfv <sebastienlfv.pro@gmail.com>
2022-04-03 14:17:04 +0200

    supprimer item panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -12,74 +12,77 @@ const priceElem = document.getElementById('totalPrice')
 let totalPrice = 0
 let nbArticles = 0
 
-products.forEach(product => {
+function displayCart() {
+    products.forEach(product => {
 
-    nbArticles += product.quantity
-    totalPrice += product.info.price
-
-    const cartItem = document.createElement('article')
-    cartItem.classList.add('cart__item')
-    cartItems.appendChild(cartItem)
-
-    const itemImgKanapCart = document.createElement('div')
-    itemImgKanapCart.classList.add('cart__item__img')
-    cartItem.appendChild(itemImgKanapCart)
-
-    const imgItemKanapCart = document.createElement('img')
-    imgItemKanapCart.src = product.info.imageUrl
-    itemImgKanapCart.appendChild(imgItemKanapCart)
+        nbArticles += product.quantity
+        totalPrice += product.info.price
     
-    const carteItemContent = document.createElement('div')
-    carteItemContent.classList.add('cart__item__content')
-    cartItem.appendChild(carteItemContent)
+        const cartItem = document.createElement('article')
+        cartItem.classList.add('cart__item')
+        cartItems.appendChild(cartItem)
+    
+        const itemImgKanapCart = document.createElement('div')
+        itemImgKanapCart.classList.add('cart__item__img')
+        cartItem.appendChild(itemImgKanapCart)
+    
+        const imgItemKanapCart = document.createElement('img')
+        imgItemKanapCart.src = product.info.imageUrl
+        itemImgKanapCart.appendChild(imgItemKanapCart)
+        
+        const carteItemContent = document.createElement('div')
+        carteItemContent.classList.add('cart__item__content')
+        cartItem.appendChild(carteItemContent)
+    
+        const carteItemContentDescription = document.createElement('div')
+        carteItemContentDescription.classList.add('cart__item__content__description')
+        carteItemContent.appendChild(carteItemContentDescription)
+    
+        const contentDescriptionTitre = document.createElement('h2')
+        contentDescriptionTitre.innerHTML = product.info.name
+        carteItemContentDescription.appendChild(contentDescriptionTitre)
+    
+        const contentDescriptionCouleur = document.createElement('p')
+        contentDescriptionCouleur.innerHTML = product.selectedVariant
+        carteItemContentDescription.appendChild(contentDescriptionCouleur)
+    
+        const contentDescriptionPrice = document.createElement('p')
+        contentDescriptionPrice.innerHTML = product.info.price
+        carteItemContentDescription.appendChild(contentDescriptionPrice)
+    
+        const carteItemContentSettings = document.createElement('div')
+        carteItemContentSettings.classList.add('cart__item__content__settings')
+        carteItemContent.appendChild(carteItemContentSettings)
+    
+        const contentSettingsQuantity = document.createElement('div')
+        contentSettingsQuantity.classList.add('cart__item__content__settings__quantity')
+        carteItemContentSettings.appendChild(contentSettingsQuantity)
+    
+        const contentSettingsQuantityP = document.createElement('p')
+        contentSettingsQuantityP.innerHTML = 'Qté :'
+        contentSettingsQuantity.appendChild(contentSettingsQuantityP)
+    
+        const contentSettingsQuantityInput = document.createElement('input')
+        contentSettingsQuantityInput.type = 'number'
+        contentSettingsQuantityInput.name = 'itemQuantity'
+        contentSettingsQuantityInput.min = '1'
+        contentSettingsQuantityInput.max = '100'
+        contentSettingsQuantityInput.value = product.quantity
+        contentSettingsQuantityInput.classList.add('itemQuantity')
+        contentSettingsQuantity.appendChild(contentSettingsQuantityInput)
+    
+        const contentSettingsDelete = document.createElement('div')
+        contentSettingsDelete.classList.add('cart__item__content__settings__delete')
+        carteItemContentSettings.append(contentSettingsDelete)
+    
+        const contentSettingsDeleteP = document.createElement('p')
+        contentSettingsDeleteP.classList.add('deleteItem')
+        contentSettingsDeleteP.innerHTML = "Supprimer"
+        contentSettingsDelete.appendChild(contentSettingsDeleteP)
+    });
+}
 
-    const carteItemContentDescription = document.createElement('div')
-    carteItemContentDescription.classList.add('cart__item__content__description')
-    carteItemContent.appendChild(carteItemContentDescription)
-
-    const contentDescriptionTitre = document.createElement('h2')
-    contentDescriptionTitre.innerHTML = product.info.name
-    carteItemContentDescription.appendChild(contentDescriptionTitre)
-
-    const contentDescriptionCouleur = document.createElement('p')
-    contentDescriptionCouleur.innerHTML = product.selectedVariant
-    carteItemContentDescription.appendChild(contentDescriptionCouleur)
-
-    const contentDescriptionPrice = document.createElement('p')
-    contentDescriptionPrice.innerHTML = product.info.price
-    carteItemContentDescription.appendChild(contentDescriptionPrice)
-
-    const carteItemContentSettings = document.createElement('div')
-    carteItemContentSettings.classList.add('cart__item__content__settings')
-    carteItemContent.appendChild(carteItemContentSettings)
-
-    const contentSettingsQuantity = document.createElement('div')
-    contentSettingsQuantity.classList.add('cart__item__content__settings__quantity')
-    carteItemContentSettings.appendChild(contentSettingsQuantity)
-
-    const contentSettingsQuantityP = document.createElement('p')
-    contentSettingsQuantityP.innerHTML = 'Qté :'
-    contentSettingsQuantity.appendChild(contentSettingsQuantityP)
-
-    const contentSettingsQuantityInput = document.createElement('input')
-    contentSettingsQuantityInput.type = 'number'
-    contentSettingsQuantityInput.name = 'itemQuantity'
-    contentSettingsQuantityInput.min = '1'
-    contentSettingsQuantityInput.max = '100'
-    contentSettingsQuantityInput.value = product.quantity
-    contentSettingsQuantityInput.classList.add('itemQuantity')
-    contentSettingsQuantity.appendChild(contentSettingsQuantityInput)
-
-    const contentSettingsDelete = document.createElement('div')
-    contentSettingsDelete.classList.add('cart__item__content__settings__delete')
-    carteItemContentSettings.append(contentSettingsDelete)
-
-    const contentSettingsDeleteP = document.createElement('p')
-    contentSettingsDeleteP.classList.add('deleteItem')
-    contentSettingsDeleteP.innerHTML = "Supprimer"
-    contentSettingsDelete.appendChild(contentSettingsDeleteP)
-
-
+function removeItemCart() {
     let btn_supprimer = document.getElementsByClassName('deleteItem');
     console.log(btn_supprimer)
 
@@ -92,9 +95,13 @@ products.forEach(product => {
             console.log('New Liste', products)
 
             localStorage.setItem('panier', JSON.stringify(products))
+            window.location.reload();
         })
     }
-});
+}
+
+displayCart();
+removeItemCart();
 
 
 articlesElem.innerHTML = nbArticles
