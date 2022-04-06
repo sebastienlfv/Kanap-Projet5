c736c53cb1ca74428af6f89c995675f7bb560ab3
sebastienlfv <sebastienlfv.pro@gmail.com>
2022-04-06 17:42:53 +0200

    *

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,4 @@
-const URL_API = "http://www.localhost:3000/api/products/"
+const URL_API = "http://localhost:3000/api/products/order"
 
 
 
@@ -47,7 +47,7 @@ function displayCart() {
         carteItemContentDescription.appendChild(contentDescriptionCouleur)
     
         const contentDescriptionPrice = document.createElement('p')
-        contentDescriptionPrice.innerHTML = product.info.price
+        contentDescriptionPrice.innerHTML = product.info.price + ' €'
         carteItemContentDescription.appendChild(contentDescriptionPrice)
     
         const carteItemContentSettings = document.createElement('div')
@@ -99,6 +99,68 @@ function removeItemCart() {
         })
     }
 }
+
+function sendOrder(order) {
+    fetch(URL_API, {
+        method: "POST",
+        headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+        body: JSON.stringify(order)
+    })
+    .then(function(res){
+        if (res.ok) {
+            console.log('res.ok',res.ok);
+            return res.json();
+        }
+    })
+    .then(function(value){
+        console.log(value);
+        // récupérer l'id qui est envoyer par l'API
+        // vider le localStorage
+        // faire un redirect vers la page de commande en mettant en paramètre l'id
+    })
+    console.log("sendOrder");
+}
+
+const formulaireDeCommande = document.querySelector('.cart__order__form')
+
+formulaireDeCommande.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+
+    
+    const formData = new FormData(formulaireDeCommande)
+    const prenom = formData.get('firstName')
+    const nom = formData.get('lastName')
+    const adress = formData.get('address')
+    const ville = formData.get('city')
+    const email = formData.get('email')
+
+
+
+
+    const informationFormulaire = {prenom, nom, adress, ville, email}
+
+    localStorage.setItem('Formulaire', JSON.stringify(informationFormulaire))
+
+    // récuperer les id des produits et les mettres dans un tableau
+
+    const productsID = products.info._id
+    console.log('productsID',productsID);
+
+    //
+
+    const payload = {contact: informationFormulaire, produits: products}
+    console.log('Payload',payload);
+    sendOrder(payload);
+    
+    console.log("Information Formulaire",informationFormulaire);
+
+    // console.log('Information Client', { prenom, nom, adress, ville, email}, products);
+})
+
+// const getPrenom = document.getElementById('firstName').value = localStorage.getItem()
+
+
 
 displayCart();
 removeItemCart();
